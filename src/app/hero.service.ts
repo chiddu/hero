f9066a5fa997caf4285111cb874d18ed92791019
S Chidambaran <chidambaran.subramanian@incred.com>
2018-02-12 19:40:39 +0530

    Anohter bucnh of changs

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,16 +4,25 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+import { MessageService } from './message.service';
+
+
 
 @Injectable()
 export class HeroService {
-
+/*
 getHeroes(): Observable<Hero[]> {
   return of(HEROES);
 	}
+	*/
 
+	getHeroes(): Observable<Hero[]> {
+	  // Todo: send the message _after_ fetching the heroes
+		  this.messageService.add('HeroService: fetched heroes');
+			  return of(HEROES);
+				}
 
+constructor(private messageService: MessageService) { }
 
-  constructor() { }
 
 }
