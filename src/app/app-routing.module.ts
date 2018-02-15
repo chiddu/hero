6897532678b24eb8a579f423ebe2005e3d7713c8
S Chidambaran <chidambaran.subramanian@incred.com>
2018-02-15 12:15:55 +0530

    Added app.coponnt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent }      from './heroes/heroes.component';
+
 
 @NgModule({
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+	];
+
+	imports: [ RouterModule.forRoot(routes) ];
 	exports : [ RouterModule ]
 })
 export class AppRoutingModule { }
