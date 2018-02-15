b38526ef3361691b33aebe7fd082231d552c6f86
S Chidambaran <chidambaran.subramanian@incred.com>
2018-02-15 13:18:50 +0530

    Completd the code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 
-@NgModule({
 const routes: Routes = [
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 	];
 
-	imports: [ RouterModule.forRoot(routes) ];
+
+@NgModule({
+
+	imports: [ RouterModule.forRoot(routes) ],
 	exports : [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,41 +10,20 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-	heroes : Hero[];
-
-	hero: Hero = {
-		id : 1,
-		name : 'WindStorm'
-	};
-
-
-	selectedHero: Hero ;
-
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
-
-	constructor(private heroService: HeroService) { }
-
-
-
-getHeroes(): void {
-  this.heroService.getHeroes()
-			.subscribe(heroes => this.heroes = heroes);
-}
-
-
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-	  this.getHeroes();
-
-//		selectedHero = { 
-//			id : 3,
-//			name : "Ada" 
-//		};
+    this.getHeroes();
   }
 
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
+
